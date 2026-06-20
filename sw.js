378671ec5929f6a4ef944fac2d5bdf56c7bb0ddd
maloshaiwy <m3aath@hotmail.com>
2026-06-20 22:21:36 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'lawha-ref-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192x192.png', './icon-512x512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(names => Promise.all(
+    names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
